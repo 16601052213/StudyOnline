c35ca4f114007d2059a5162800f5a63fb430523d
zhuhai <16601052213@163.com>
2020-05-08 23:58:45 +0800

    前台首页和登录页面的配置

--- mkonline/urls.py
+++ mkonline/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 import xadmin
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
+
+    url('^$', TemplateView.as_view(template_name="index.html"), name="index"),
+    url('^login/$', TemplateView.as_view(template_name="login.html"), name="login")
+
 ]
