812d7703655284a8796db3086c158a28534b3bf7
zhuhai <16601052213@163.com>
2020-05-20 23:13:19 +0800

    课程推荐

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -7,7 +7,8 @@ from django.views.generic import View
 from pure_pagination import Paginator
 
 from courses.models import Course, CourseResource
-from operation.models import UserFavorite, CourseComments
+from operation.models import UserFavorite, CourseComments, UserCourse
+from utils.mixin_utils import LoginRequiredMixin
 
 
 class CourseListView(View):
@@ -30,7 +31,7 @@ class CourseListView(View):
         except PageNotAnInteger:
             page = 1
 
-        p = Paginator(all_courses, 3, request=request)
+        p = Paginator(all_courses, 6, request=request)
 
         courses = p.page(page)
 
@@ -73,21 +74,37 @@ class CourseDetailView(View):
         })
 
 
-class CourseInfoView(View):
+class CourseInfoView(LoginRequiredMixin, View):
     """
     课程章节信息
     """
 
     def get(self, request, course_id):
         course = Course.objects.get(id=int(course_id))
+
+        # 查询用户是否已经关联了该课程
+        user_courses = UserCourse.objects.filter(user=request.user, course=course)
+        if not user_courses:
+            user_course = UserCourse(user=request.user, course=course)
+            user_course.save()
+
         all_resources = CourseResource.objects.filter(course=course)
+        user_cousers = UserCourse.objects.filter(course=course)
+        user_ids = [user_couser.user.id for user_couser in user_cousers]
+        all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
+        # 取出所有课程id
+        course_ids = [user_couser.course.id for user_couser in user_cousers]
+        # 获取该用户学过其他的所有课程
+        relate_course = Course.objects.filter(id__in=course_ids).order_by("-click_nums")[:5]
+
         return render(request, "course-video.html", {
             "course": course,
             "course_resources": all_resources,
+            "relate_course": relate_course
         })
 
 
-class CommentsView(View):
+class CommentsView(LoginRequiredMixin, View):
     """
     课程评论功能
     """
